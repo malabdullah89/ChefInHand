d3ea02820250a44ef66d451d63ec57db02063c4c
malabdullah89 <53144490+malabdullah89@users.noreply.github.com>
2021-01-09 23:38:15 +0300

    test2

--- contentsManagement/forms.py
+++ contentsManagement/forms.py
@@ -233,13 +233,9 @@ class AddType(forms.ModelForm):
     ))
 
     class Meta:
-        model = models.Type
-        fields = [
-            'type_english_name',
-            'type_arabic_name',
-            'type_hindi_name',
-
-        ]
+        class Meta:
+            model = models.Type
+            exclude = ['created_by', ]
 
 
 class AddIngredientForm(forms.ModelForm):
